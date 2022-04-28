90c1550fa5372f41fdd75e0e17676585638e1f8c
MaraLuque <58600405+Maraluque@users.noreply.github.com>
2022-04-28 10:05:51 +0200

    Delete src/src directory

--- src/src/js/scripts.js
+++
@@ -1,8 +0,0 @@
-//Ejemplo de como pueden programar desde un JS externo.
-
-//Cada que seleccionen un producto en la lista, se muestra su valor en un alert.
-async function cambio(){
-    let items = document.getElementById('menuRecetas');
-    alert("Apretado")
-    
-}
