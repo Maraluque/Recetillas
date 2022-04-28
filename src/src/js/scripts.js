1976ca38a0a51755bc76a747a99b18e533d09666
MaraLuque <58600405+Maraluque@users.noreply.github.com>
2022-04-28 09:34:34 +0200

    Pie de página terminado

---
+++ src/src/js/scripts.js
@@ -0,0 +1,8 @@
+//Ejemplo de como pueden programar desde un JS externo.
+
+//Cada que seleccionen un producto en la lista, se muestra su valor en un alert.
+async function cambio(){
+    let items = document.getElementById('menuRecetas');
+    alert("Apretado")
+    
+}
